377d01314158defe4a35dfb755d70546f91fc942
ekolasky <49255176+ekolasky@users.noreply.github.com>
2024-09-01 13:27:17 -0700

    Added arguments

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -89,12 +89,14 @@ class OptimizationParams(ParamGroup):
         self.lambda_dssim = 0.2
         self.densification_interval = 100
         self.opacity_reset_interval = 3000
-        self.densify_from_iter = 500
+        self.densify_from_iter = 100
         self.densify_until_iter = 15_000
         self.densify_grad_threshold = 0.0002
         self.depth_l1_weight_init = 1.0
         self.depth_l1_weight_final = 0.01
         self.random_background = False
+        self.max_num_points = 2048
+        self.pruning_ratio = 0.5
         super().__init__(parser, "Optimization Parameters")
 
 def get_combined_args(parser : ArgumentParser):
